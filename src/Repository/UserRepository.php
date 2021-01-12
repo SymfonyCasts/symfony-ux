c05c3c3d9ca70566fc73dff5fde56ebf95ee28d2
Ryan Weaver <ryan@thatsquality.com>
2021-01-12 13:15:12 -0500

    updating ManagerRegistry

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
