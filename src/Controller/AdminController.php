d9060495e0a1899f3664ad85177d19623c4e87c4
Ryan Weaver <ryan@thatsquality.com>
2021-01-21 12:45:21 -0500

    adding admin dashboard and tweaking CSS and templates to fit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_dashboard")
+     */
+    public function dashboard()
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+}
