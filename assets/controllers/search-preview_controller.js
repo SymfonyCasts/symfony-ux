5711bfbdc3c97a1d01985d62f14da160e7dbafe0
Ryan Weaver <ryan@thatsquality.com>
2021-02-03 14:35:54 -0500

    Step 44: SearchPreview: add url value

--- assets/controllers/search-preview_controller.js
+++ assets/controllers/search-preview_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
+    static values = {
+        url: String,
+    }
+
     onSearchKeyUp(event) {
-        console.log(event);
+        console.log(this.urlValue);
     }
 }
