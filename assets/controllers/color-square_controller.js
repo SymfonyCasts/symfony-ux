ef48021f3e9a0c31b5ff9e44c89562108ad644fa
Ryan Weaver <ryan@thatsquality.com>
2021-02-03 14:35:48 -0500

    Step 25: ColorSelector: add "selected" class

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -2,6 +2,6 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
     selectColor(event) {
-        console.log(event);
+        event.currentTarget.classList.add('selected');
     }
 }
