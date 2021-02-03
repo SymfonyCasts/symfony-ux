67fcd45d6f79bbc4f446b9f6e46b2d58be891f21
Ryan Weaver <ryan@thatsquality.com>
2021-02-03 14:35:41 -0500

    Step 1: InstallEncore: install encore

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
