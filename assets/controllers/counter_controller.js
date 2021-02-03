eb25a8f593673f70c1b9dd8bee5f966fb020fd00
Ryan Weaver <ryan@thatsquality.com>
2021-02-03 14:35:45 -0500

    Step 13: Targets: add span element

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -2,12 +2,14 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
     connect() {
-        this.element.innerHTML = 'You have clicked me 0 times 😢';
         this.count = 0;
+
+        const counterNumberElement = this.element
+            .getElementsByClassName('counter-count')[0];
 
         this.element.addEventListener('click', () => {
             this.count++;
-            this.element.innerHTML = this.count;
+            counterNumberElement.innerText = this.count;
         });
     }
 }
