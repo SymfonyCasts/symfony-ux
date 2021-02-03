869f2b7a32f3a2950053edc482a1b213e40f0626
Ryan Weaver <ryan@thatsquality.com>
2021-02-03 14:35:47 -0500

    Step 22: ColorSelector: add action to squares

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-    connect() {
-        console.log(this.element.innerHTML);
+    selectColor(event) {
+        console.log(event);
     }
 }
