a8e21b6f0b52eb306247cc64566db6a6c550c823
Ryan Weaver <ryan@thatsquality.com>
2021-02-03 14:36:04 -0500

    Step 76: Laziness: dynamic import react-dom

--- assets/controllers/made-with-love_controller.js
+++ assets/controllers/made-with-love_controller.js
@@ -1,13 +1,14 @@
 import { Controller } from 'stimulus';
-import ReactDOM from 'react-dom';
 import React from 'react';
 import MadeWithLove from '../components/MadeWithLove';
 
 export default class extends Controller {
     connect() {
-        ReactDOM.render(
-            <MadeWithLove />,
-            this.element
-        )
+        import('react-dom').then((ReactDOM) => {
+            ReactDOM.default.render(
+                <MadeWithLove />,
+                this.element
+            )
+        })
     }
 }
