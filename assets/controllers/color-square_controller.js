530c8051b6195693663e63c30f465e2948d1dc43
Ryan Weaver <ryan@thatsquality.com>
2021-02-03 14:35:49 -0500

    Step 26: ColorSelector: colorSquare target

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
+    targets = ['colorSquare']
+
     selectColor(event) {
+        console.log(this.colorSquareTargets);
+
         event.currentTarget.classList.add('selected');
     }
 }
