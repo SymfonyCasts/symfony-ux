8f1f2420394c186794b5c50dbd79b9e13dfed81e
Ryan Weaver <ryan@thatsquality.com>
2021-02-03 14:35:53 -0500

    Step 41: Values: use value exclusively

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,8 +1,6 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-    selectedColorId = null;
-
     static targets = ['colorSquare', 'select']
     static values = {
         colorId: Number
@@ -13,7 +11,7 @@ export default class extends Controller {
     }
 
     selectColor(event) {
-        this.setSelectedColor(event.currentTarget.dataset.colorId)
+        this.colorIdValue = event.currentTarget.dataset.colorId;
     }
 
     colorIdValueChanged() {
@@ -26,32 +24,5 @@ export default class extends Controller {
                 element.classList.remove('selected');
             }
         });
-    }
-
-    setSelectedColor(newColorId) {
-        if (newColorId === this.selectedColorId) {
-            this.findSelectedColorSquare().classList.remove('selected');
-
-            this.selectedColorId = null;
-            this.selectTarget.value = '';
-
-            return;
-        }
-
-        this.selectedColorId = newColorId;
-
-        this.colorSquareTargets.forEach((element) => {
-            element.classList.remove('selected');
-        });
-
-        this.findSelectedColorSquare().classList.add('selected');
-        this.selectTarget.value = this.selectedColorId;
-    }
-
-    /**
-     * @return {Element|null}
-     */
-    findSelectedColorSquare() {
-        return this.colorSquareTargets.find((element) => element.dataset.colorId == this.selectedColorId);
     }
 }
