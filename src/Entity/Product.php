d3573eee177f226215fab1dc00d5dda767fac663
Ryan Weaver <ryan@thatsquality.com>
2021-03-15 14:17:10 -0400

    adding validation rules to Product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
@@ -23,6 +24,7 @@ class Product
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups("product:read")
+     * @Assert\NotBlank()
      */
     private $name;
 
@@ -45,11 +47,14 @@ class Product
     /**
      * @ORM\Column(type="integer")
      * @Groups("product:read")
+     * @Assert\GreaterThan(0)
+     * @Assert\NotBlank()
      */
     private $price;
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\GreaterThanOrEqual(0)
      */
     private $stockQuantity;
 
