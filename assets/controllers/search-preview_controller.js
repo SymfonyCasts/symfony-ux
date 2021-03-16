d6dda8369d131ee6b2c163cb82080f8251d58f54
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:49 -0400

    [tuts-apply-diff] Applying existing diff file for step searchpreview-add-url-value

--- assets/controllers/search-preview_controller.js
+++ assets/controllers/search-preview_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
+    static values = {
+        url: String,
+    }
+
     onSearchInput(event) {
-        console.log(event);
+        console.log(this.urlValue);
     }
 }
