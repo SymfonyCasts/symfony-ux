31d9aa6d08eaaa75cc8945c5d6519fe24b958c53
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:49 -0400

    [tuts-apply-diff] Applying existing diff file for step searchpreview-add-input-action

--- assets/controllers/search-preview_controller.js
+++ assets/controllers/search-preview_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-    connect() {
-        console.log('connected!');
+    onSearchInput(event) {
+        console.log(event);
     }
 }
