26e6363281d7cdf7b205c7a1c2060d3f45860ea9
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:55 -0400

    [tuts-apply-diff] Applying existing diff file for step react-enable-the-preset

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ Encore
     //.enableTypeScriptLoader()
 
     // uncomment if you use React
-    //.enableReactPreset()
+    .enableReactPreset()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
