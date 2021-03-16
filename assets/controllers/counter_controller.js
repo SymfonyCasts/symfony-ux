3fa75e696c15f15b3aa6fcfa366ef9c0e8f33ef6
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:43 -0400

    [tuts-apply-diff] Applying existing diff file for step targets-add-target

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -1,15 +1,14 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
+    static targets = ['count'];
+
     connect() {
         this.count = 0;
 
-        const counterNumberElement = this.element
-            .getElementsByClassName('counter-count')[0];
-
         this.element.addEventListener('click', () => {
             this.count++;
-            counterNumberElement.innerText = this.count;
+            this.countTarget.innerText = this.count;
         });
     }
 }
