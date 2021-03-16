c89f0966509770b2c84bd16f85fce41dd448c7cc
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:54 -0400

    [tuts-apply-diff] Applying existing diff file for step chartjs-render-chart

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,14 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin_dashboard")
      */
-    public function dashboard()
+    public function dashboard(ChartBuilderInterface $chartBuilder)
     {
-        return $this->render('admin/dashboard.html.twig');
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'Sales!',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [522, 1500, 2250, 2197, 2345, 3122, 3099],
+                ],
+            ],
+        ]);
+
+        return $this->render('admin/dashboard.html.twig', [
+            'chart' => $chart
+        ]);
     }
 }
