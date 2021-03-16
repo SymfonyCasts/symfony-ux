711dea42d1d5586b3fb6f049e265f6431e546fe6
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:55 -0400

    [tuts-apply-diff] Applying existing diff file for step chartjsextend-change-chart-in-5-seconds

--- assets/controllers/admin-chartjs_controller.js
+++ assets/controllers/admin-chartjs_controller.js
@@ -2,6 +2,15 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
     onChartConnect(event) {
-        console.log(event);
+        this.chart = event.detail.chart;
+
+        setTimeout(() => {
+            this.setNewData();
+        }, 5000)
+    }
+
+    setNewData() {
+        this.chart.data.datasets[0].data[2] = 30;
+        this.chart.update();
     }
 }
