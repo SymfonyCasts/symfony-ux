f9e3261e8d63415fbda61cd92016634115d40c56
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:46 -0400

    [tuts-apply-diff] Applying existing diff file for step colorselector-removing-border-on-others

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -4,7 +4,9 @@ export default class extends Controller {
     static targets = ['colorSquare']
 
     selectColor(event) {
-        console.log(this.colorSquareTargets);
+        this.colorSquareTargets.forEach((element) => {
+            element.classList.remove('selected');
+        });
 
         event.currentTarget.classList.add('selected');
     }
