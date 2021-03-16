ae243e5c4b0ba3adab686db904c201913aa2e56f
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:51 -0400

    [tuts-apply-diff] Applying existing diff file for step sweetalert-paste-in-swal

--- assets/controllers/submit-confirm_controller.js
+++ assets/controllers/submit-confirm_controller.js
@@ -1,8 +1,27 @@
 import { Controller } from 'stimulus';
+import Swal from 'sweetalert2';
 
 export default class extends Controller {
     onSubmit(event) {
         event.preventDefault();
-        console.log(event);
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success',
+                )
+            }
+        })
+
     }
 }
