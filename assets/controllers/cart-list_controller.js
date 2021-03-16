8562d960a0b7970a9413d6501810e527541f0f66
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:54 -0400

    [tuts-apply-diff] Applying existing diff file for step 2ndcontroller-css-fade-out

--- assets/controllers/cart-list_controller.js
+++ assets/controllers/cart-list_controller.js
@@ -6,6 +6,8 @@ export default class extends Controller {
     }
 
     async removeItem(event) {
+        event.currentTarget.classList.add('removing');
+
         const response = await fetch(this.cartRefreshUrlValue);
         this.element.innerHTML = await response.text();
     }
