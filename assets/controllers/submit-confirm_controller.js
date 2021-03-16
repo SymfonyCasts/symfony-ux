33e9059d697c6890c2e11ffea909b1d0046e8beb
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:52 -0400

    [tuts-apply-diff] Applying existing diff file for step sweetalert-make-configurable

--- assets/controllers/submit-confirm_controller.js
+++ assets/controllers/submit-confirm_controller.js
@@ -2,17 +2,24 @@ import { Controller } from 'stimulus';
 import Swal from 'sweetalert2';
 
 export default class extends Controller {
+    static values = {
+        title: String,
+        text: String,
+        icon: String,
+        confirmButtonText: String,
+    }
+
     onSubmit(event) {
         event.preventDefault();
 
         Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
+            title: this.titleValue || null,
+            text: this.textValue || null,
+            icon: this.iconValue || null,
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!',
+            confirmButtonText: this.confirmButtonTextValue || 'Yes',
         }).then((result) => {
             if (result.isConfirmed) {
                 this.element.submit();
