b2d4f189e060598640c335830f5fdffc54bf7b9a
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:42 -0400

    [tuts-apply-diff] Applying existing diff file for step controllerbasics-increment-on-click

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -3,5 +3,11 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
     connect() {
         this.element.innerHTML = 'You have clicked me 0 times 😢';
+        this.count = 0;
+
+        this.element.addEventListener('click', () => {
+            this.count++;
+            this.element.innerHTML = this.count;
+        });
     }
 }
