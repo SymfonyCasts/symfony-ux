fdd602b3e5977334d37d80e8b102b9cfaf3ade66
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:44 -0400

    [tuts-apply-diff] Applying existing diff file for step actions-remove-connect

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -1,11 +1,8 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
+    count = 0;
     static targets = ['count'];
-
-    connect() {
-        this.count = 0;
-    }
 
     increment() {
         this.count++;
