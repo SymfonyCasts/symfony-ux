8a18fac3b03fded2e4fa76fe5c38cf12992d1d2b
Ryan Weaver <ryan@thatsquality.com>
2021-03-16 14:25:53 -0400

    [tuts-apply-diff] Applying existing diff file for step 2ndcontroller-listen-to-event

--- assets/controllers/cart-list_controller.js
+++ assets/controllers/cart-list_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-    connect() {
-        console.log('🛒');
+    removeItem(event) {
+        console.log(event.currentTarget);
     }
 }
