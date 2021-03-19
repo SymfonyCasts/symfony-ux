6314a5ea606fd4146bc8ddf720b5bb2f7d5afa9c
Ryan Weaver <ryan@thatsquality.com>
2021-03-19 14:33:52 -0400

    [tuts-apply-diff] Applying existing diff file for step modal-add-formurl-value

--- assets/controllers/modal-form_controller.js
+++ assets/controllers/modal-form_controller.js
@@ -3,8 +3,12 @@ import { Modal } from 'bootstrap';
 
 export default class extends Controller {
     static targets = ['modal'];
+    static values = {
+        formUrl: String,
+    }
 
     openModal(event) {
+        console.log(this.formUrlValue);
         const modal = new Modal(this.modalTarget, {});
         modal.show();
     }
