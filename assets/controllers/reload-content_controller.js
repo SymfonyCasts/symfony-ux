b140004cfeed8d43f2a0d31611318e9805f10bd5
Ryan Weaver <ryan@thatsquality.com>
2021-03-19 14:33:56 -0400

    [tuts-apply-diff] Applying existing diff file for step reloadcontroller-opacity-trick

--- assets/controllers/reload-content_controller.js
+++ assets/controllers/reload-content_controller.js
@@ -7,7 +7,9 @@ export default class extends Controller {
     }
 
     async refreshContent(event) {
+        this.contentTarget.style.opacity = .5;
         const response = await fetch(this.urlValue);
         this.contentTarget.innerHTML = await response.text();
+        this.contentTarget.style.opacity = 1;
     }
 }
