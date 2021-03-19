236d47a2038df89a41efd35284035c872bd45c94
Ryan Weaver <ryan@thatsquality.com>
2021-03-19 14:33:57 -0400

    [tuts-apply-diff] Applying existing diff file for step 3rdparty-register-controller

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,8 +1,12 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
+import { Autocomplete } from 'stimulus-autocomplete';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
+const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
     /\.(j|t)sx?$/
 ));
+app.register('autocomplete', Autocomplete);
+
+export { app };
