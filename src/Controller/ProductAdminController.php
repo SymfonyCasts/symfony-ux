0f622112bda8c696b234d68f228dbcd20dba718b
Ryan Weaver <ryan@thatsquality.com>
2021-03-19 14:33:54 -0400

    [tuts-apply-diff] Applying existing diff file for step asyncform-return-422-or-200

--- src/Controller/ProductAdminController.php
+++ src/Controller/ProductAdminController.php
@@ -39,6 +39,10 @@ class ProductAdminController extends AbstractController
             $entityManager->persist($product);
             $entityManager->flush();
 
+            if ($request->isXmlHttpRequest()) {
+                return new Response(null, 204);
+            }
+
             return $this->redirectToRoute('product_admin_index');
         }
 
@@ -47,7 +51,10 @@ class ProductAdminController extends AbstractController
         return $this->render('product_admin/' . $template, [
             'product' => $product,
             'form' => $form->createView(),
-        ]);
+        ], new Response(
+            null,
+            $form->isSubmitted() && !$form->isValid() ? 422 : 200,
+        ));
     }
 
     /**
