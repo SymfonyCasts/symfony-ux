3b3bb6343fb4cfe41e8c61ea3486b58204e00bf9
Ryan Weaver <ryan@thatsquality.com>
2021-03-24 15:28:29 -0400

    tuts hero working...

--- assets/controllers/reload-content_controller.js
+++ assets/controllers/reload-content_controller.js
@@ -7,9 +7,11 @@ export default class extends Controller {
     }
 
     async refreshContent(event) {
-        this.contentTarget.style.opacity = .5;
+        const target = this.hasContentTarget ? this.contentTarget : this.element;
+
+        target.style.opacity = .5;
         const response = await fetch(this.urlValue);
-        this.contentTarget.innerHTML = await response.text();
-        this.contentTarget.style.opacity = 1;
+        target.innerHTML = await response.text();
+        target.style.opacity = 1;
     }
 }
