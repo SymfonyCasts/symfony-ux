697571fb06d36fb4a362cdfbb19b942d1e98e54f
Ryan Weaver <ryan@thatsquality.com>
2021-04-15 10:31:46 -0400

    tuts hero working...

--- assets/controllers/autocomplete-transition_controller.js
+++ assets/controllers/autocomplete-transition_controller.js
@@ -2,6 +2,8 @@ import { Controller } from 'stimulus';
 import { addFadeTransition } from '../util/add-transition';
 
 export default class extends Controller {
+    static targets = ['results'];
+
     connect() {
         addFadeTransition(this, this.resultsTarget);
     }
