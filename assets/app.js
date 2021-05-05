889a3b6733b7ab60b34b2aadfffbd75f1e4044d2
Ryan Weaver <ryan@thatsquality.com>
2021-05-05 13:02:36 -0400

    adding some play javascript

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,10 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import $ from 'jquery';
+
+$(document).ready(() => {
+    console.log('page is ready!');
+})
+console.log('script is done!');
