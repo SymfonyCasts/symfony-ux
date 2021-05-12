e5d35e19d57b859965c3b274995bec5ee45e5699
Ryan Weaver <ryan@thatsquality.com>
2021-05-12 14:56:02 -0400

    securing product admin

--- src/Controller/ProductAdminController.php
+++ src/Controller/ProductAdminController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/admin/product")
+ * @IsGranted("ROLE_ADMIN")
  */
 class ProductAdminController extends AbstractController
 {
