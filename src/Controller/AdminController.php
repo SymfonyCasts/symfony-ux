4672d42285e08cd070d2ae34bea7559c28749901
cybernet <cybernet2u@yahoo.com>
2022-09-25 07:40:16 +0100

    use Response
    Symfony\Component\HttpFoundation\Response

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -10,7 +11,7 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin_dashboard")
      */
-    public function dashboard()
+    public function dashboard(): Response
     {
         return $this->render('admin/dashboard.html.twig');
     }
